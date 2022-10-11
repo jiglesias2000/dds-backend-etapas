291569e9fcf66a844f1761eb10656083f41fc04d
jiglesias2000 <jiglesias.2000@gmail.com>
2022-10-10 22:34:24 -0300

    Etapa 2 completa

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 // crear servidor
 const app = express();
+app.use(express.json()); // para poder leer json en el body
 
 // controlar ruta
 app.get("/", (req, res) => {
   res.send("Backend inicial DDS-EXPRESS!");
 });
+
+
+const articulosfamiliasmockRouter = require("./routes/articulosfamiliasmock");
+app.use(articulosfamiliasmockRouter);
+
 
 // levantar servidor
 const port = 3000;
